1e14b5fe7d1cd14d52f18a7c1ad84946e02852c8
KetsanaMonday <ketsanamonday@gmail.com>
2025-01-13 15:11:07 +0700

    Add files via upload

---
+++ routers/login.routers.js
@@ -0,0 +1,14 @@
+const User = require('../contorllers/login.contorllers');
+const router = require('express').Router();
+
+
+router.get("/users/read",User.readUsers);
+router.post("/users/create",User.createUsers);
+router.post("/users/update",User.updateUsers);
+router.get("/users/read1",User.readUsers1);
+router.post("/users/login",User.userLogin);
+
+router.post("/users/register",User.register);
+router.post("/users/checkuser",User.checkUser1);
+router.post("/users/checktoken",User.checkToken);
+module.exports=router;
